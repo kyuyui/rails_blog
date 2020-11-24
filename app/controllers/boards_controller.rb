61bca998dcb680acf694518c5bb66cf99ca1ef56
kyu__yui <chiuyuf61139@gmail.com>
2020-11-25 01:05:42 +0800

    add destroy

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -18,12 +18,27 @@ class BoardsController < ApplicationController
     end
 
     def show
+        @board = Board.find_by(id: params[:id])
     end
 
     def edit
+        @board = Board.find_by(id: params[:id])
+        # 使用find_by方法，把id是params[:id]的資料抓出來，並把找到的結果存成實體變數
+    end
+
+    def update
+        @board = Board.find_by(id: params[:id])
+        if @board.update(board_params)
+            redirect_to root_path, notice: '更新成功'
+        else
+            render :edit
+        end
     end
 
     def destroy
+        @board = Board.find_by(id: params[:id])
+        @board.destroy if @board
+        redirect_to root_path, notice: '刪除成功'
     end
 
     private
