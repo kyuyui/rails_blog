1dc749cafaca35f6e4fcf55fa0cd0b1c08f389ca
kyu__yui <chiuyuf61139@gmail.com>
2020-11-26 23:18:25 +0800

    add sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   root 'boards#index'
-  #首頁
-  
+
+  get '/users/sign_in', to: 'sessions#new'
+  #登入
+	delete '/users/sign_out', to: 'sessions#destroy'
+	#登出
+  get '/users/sign_up', to: 'registrations#new'
+  #註冊
+  post '/users/sign_up', to: 'registrations#create'
+	#建立資料
+  get '/users', to: 'users#index'
+	#轉回首頁
+
   resources :boards
 end
