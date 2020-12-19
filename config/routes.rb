7f376734cf92371b35adc600887e555ec9deae9a
kyu__yui <chiuyuf61139@gmail.com>
2020-12-19 10:01:47 +0800

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,27 @@
 Rails.application.routes.draw do
   root 'boards#index'
 
-  resource :user, controller: 'registrations', only: [:creat, :edit, :update]do
+  resource :users, controller: 'registrations', only: [:creat, :edit, :update] do
     get '/sign_up', action: 'new'
   end
 
-  #get '/users/sign_up', to: 'registrations#new', as: 'registration'
-  #註冊
-  
-  post '/users/sign_up', to: 'registrations#create' 
-	#建立資料
+  # get '/users/sign_up', to: 'registrations#new', as: 'registration'
+  # #註冊
 
-  get '/users/sign_in', to: 'sessions#new', as: 'session'
-  #登入
-  post '/login', to: 'sessions#create', as: 'login'
+  # post '/users/sign_up', to: 'registrations#create' 
+	# #建立資料
 
-	delete '/logout', to: 'sessions#destroy', as: 'logout'
-  #登出
-  
-  get "/users/edit", to: "registrarion#edit", as: 'edit_registration'
-  put "/user/edit", to: "registration#update"
+  resource :users, controller: 'sessions', only: [] do
+    get '/sign_in', action: 'new'
+    post '/login', action: 'create'
+    delete '/logout', action: 'destroy'
+  end
 
-  get '/users', to: 'users#index'
-  #轉回首頁
+  # get '/users/sign_in', to: 'sessions#new', as: 'session'
+  # #登入
+  # post '/login', to: 'sessions#create', as: 'login'
+	# delete '/logout', to: 'sessions#destroy', as: 'logout'
+  # #登出
 
   resources :boards
 end
