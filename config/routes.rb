6d5bbf92f5d5b88363307a969a822ff9f6af5a29
kyu__yui <chiuyuf61139@gmail.com>
2020-12-20 18:11:51 +0800

    fix route

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,27 @@
 Rails.application.routes.draw do
   root 'boards#index'
 
-  resource :users, controller: 'registrations', only: [:creat, :edit, :update] do
-    get '/sign_up', action: 'new'
-  end
+  # resource :users, controller: 'registrations', only: [:creat, :edit, :update] do
+  #   get '/sign_up', action: 'new'
+  # end
 
-  # get '/users/sign_up', to: 'registrations#new', as: 'registration'
-  # #註冊
+  get '/users/sign_up', to: 'registrations#new', as: 'registration'
+  #註冊
 
-  # post '/users/sign_up', to: 'registrations#create' 
-	# #建立資料
+  post '/users/sign_up', to: 'registrations#create' 
+	#建立資料
 
-  resource :users, controller: 'sessions', only: [] do
-    get '/sign_in', action: 'new'
-    post '/login', action: 'create'
-    delete '/logout', action: 'destroy'
-  end
+  # resource :users, controller: 'sessions', only: [] do
+  #   get '/sign_in', action: 'new'
+  #   post '/login', action: 'create'
+  #   delete '/logout', action: 'destroy'
+  # end
 
-  # get '/users/sign_in', to: 'sessions#new', as: 'session'
-  # #登入
-  # post '/login', to: 'sessions#create', as: 'login'
-	# delete '/logout', to: 'sessions#destroy', as: 'logout'
-  # #登出
+  get '/users/sign_in', to: 'sessions#new', as: 'session'
+  #登入
+  post '/login', to: 'sessions#create', as: 'login'
+	delete '/logout', to: 'sessions#destroy', as: 'logout'
+  #登出
 
   resources :boards
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_11_26_054203) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "boards", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
