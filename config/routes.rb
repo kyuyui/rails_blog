bf8f6f34ffa5c2de9fb036ce018d9c106f3654ae
kyu__yui <chiuyuf61139@gmail.com>
2021-01-05 14:49:17 +0800

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,20 @@
 Rails.application.routes.draw do
   root 'boards#index'
 
-  # resource :users, controller: 'registrations', only: [:creat, :edit, :update] do
-  #   get '/sign_up', action: 'new'
-  # end
-
-  get '/users/sign_up', to: 'registrations#new', as: 'registration'
+  resource :users, controller: 'registrations', only: [:create, :edit, :update] do
+    get '/sign_up', action: 'new'
+  end
   #註冊
-
-  post '/users/sign_up', to: 'registrations#create' 
-	#建立資料
 
   # resource :users, controller: 'sessions', only: [] do
   #   get '/sign_in', action: 'new'
   #   post '/login', action: 'create'
   #   delete '/logout', action: 'destroy'
   # end
+  resource :users, controller: 'sessions', only: [] do
+    get '/sign_in', action: 'new'
+    post '/sign_in', action: 'create'
+  end
 
   get '/users/sign_in', to: 'sessions#new', as: 'session'
   #登入
